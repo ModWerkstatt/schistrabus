e7d47d5367b3f22360342ea43cfd05a91f2b64b3
Florian Thiel <flth@flth.de>
2023-01-28 21:18:35 +0100

    import

---
+++ mod.lua
@@ -0,0 +1,22 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+				name = "ModWerkstatt",
+				role = "CREATOR",
+			},
+		},
+		tags = { "train", "railroad", "europe", "railbus", "street", "two-way", "diesel", "bus" },
+		dependencies = { },
+		url = { },
+	},
+	options = {
+	},
+}
+end
---
+++ strings.lua
@@ -0,0 +1,49 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Railroad Bus (\"Schi-Stra-Bus\")",
+		["mod_desc"] = "The railroad bus, also called Schi-Stra-Bus for short, is a two-way vehicle for passenger transport on railway lines and roads. The Deutsche Bundesbahn used it both as a passenger train and as a rail bus.",
+
+        ["Schienenstrassenbus_menu"] = "Railroad Bus (\"Schi-Stra-Bus\")",
+        ["SchiStraBus_schiene_proto_name"] = "Schi-Stra-Bus NWF BS 300 (on track wagons) (Prototype 1952)",
+        ["SchiStraBus_schiene_name"] = "Schi-Stra-Bus NWF BS 300 (on track wagons) (1955)",
+        ["SchiStraBus_schiene_museum_name"] = "Schi-Stra-Bus NWF BS 300 (on track wagons) (Museum)",
+        ["SchiStraBus_strasse_proto_name"] = "Schi-Stra-Bus NWF BS 300 (Prototype 1952)",
+        ["SchiStraBus_strasse_name"] = "Schi-Stra-Bus NWF BS 300 (1955)",
+        ["SchiStraBus_strasse_museum_name"] = "Schi-Stra-Bus NWF BS 300 (Museum)",
+        ["SchiStraBus_desc"] = "The railroad bus, also called Schi-Stra-Bus for short, is a two-way vehicle for passenger transport on railway lines and roads. The Deutsche Bundesbahn used it both as a passenger train and as a rail bus. In this case, the so-called \"Spurwagen\" (track wagons) were used when transferring at the station. (bogies, so to speak) were attached to the front and back of the bus. This was complicated because it required a grooved rail and the bus had to be lifted by hydraulics. At least there was no need for passengers to change vehicles.\nThe vehicles were not very flexible in use. Switching between rail and road was only possible at stations that were equipped accordingly and where track wagons were available.\nSince 1976, one example has been preserved in the Museum Bochum-Dahlhausen.",
+		["street"] = "Street",
+		["track"] = "Track",
+		["sidewalk"] = "Sidewalk",
+        ["height"] = "Height",
+        ["model"] = "Model",
+        ["spwabdg"] = "Track wagon cover",
+		["SchiStraBus_tooltip"] = "Different assets to choose from:\n Picture 1 = Rail Road Bus Prototype (Rail) \n Picture 2 = Rail Road Bus Museum (Rail) \n Picture 3 = Rail Road Bus (Rail) \n Picture 4 = Rail Road Bus Track Car (Rail) \n Picture 5 = Rail Road Bus Prototype (Road) \n Picture 6 = Rail Road Bus Museum (Road) \n Picture 7 = Rail Road Bus (Road) \n.",
+
+	},
+	de = {
+
+		["mod_name"] = "Schienen-Straßen-Omnibus",
+		["mod_desc"] = "Der Schienen-Straßen-Omnibus, auch kurz Schi-Stra-Bus genannt, ist ein Zweiwegefahrzeug für den Personenverkehr auf Eisenbahnstrecken und Straßen. Die Deutsche Bundesbahn setzte ihn sowohl als Personenzug als auch als Bahnbus ein.",
+
+        ["Schienenstrassenbus_menu"] = "Schienen-Straßen-Omnnibus",
+        ["SchiStraBus_schiene_proto_name"] = "Schi-Stra-Bus NWF BS 300 (auf Spurwagen) (Prototyp 1952)",
+        ["SchiStraBus_schiene_name"] = "Schi-Stra-Bus NWF BS 300 (auf Spurwagen) (1955)",
+        ["SchiStraBus_schiene_museum_name"] = "Schi-Stra-Bus NWF BS 300 (auf Spurwagen) (Museum)",
+        ["SchiStraBus_strasse_proto_name"] = "Schi-Stra-Bus NWF BS 300 (Prototyp 1952)",
+        ["SchiStraBus_strasse_name"] = "Schi-Stra-Bus NWF BS 300 (1955)",
+        ["SchiStraBus_strasse_museum_name"] = "Schi-Stra-Bus NWF BS 300 (Museum)",
+        ["SchiStraBus_desc"] = "Der Schienen-Straßen-Omnibus, auch kurz Schi-Stra-Bus genannt, ist ein Zweiwegefahrzeug für den Personenverkehr auf Eisenbahnstrecken und Straßen. Die Deutsche Bundesbahn setzte ihn sowohl als Personenzug als auch als Bahnbus ein. Dabei wurden beim Umsetzen im Bahnhof die sogenannten \"Spurwagen\" (sozusagen Drehgestelle) vorne und hinten unter dem Bus befestigt. Dies war aufwendig, da es dazu eine Rillenschiene benötigte und der Bus mittels Hydraulik angehoben werden musste. Immerhin konnte das Umsteigen entfallen.\nDie Fahrzeuge waren wenig flexibel im Einsatz. Ein Wechsel zwischen Schiene und Straße war nur an Bahnhöfen möglich, die entsprechend ausgerüstet und an denen Spurwagen verfügbar waren.\nSeit 1976 ist ein Exemplar im Museum Bochum-Dahlhausen erhalten geblieben.",
+		["street"] = "Straße",
+		["track"] = "Schiene",
+		["sidewalk"] = "Bürgersteig",
+        ["height"] = "Höhe",
+        ["model"] = "Modell",
+        ["spwabdg"] = "Spurwagen Abdeckung",
+		["SchiStraBus_tooltip"] = "verschiende Assets zur Auswahl:\n Bild 1 = SchienenStrassenbus Prototyp (Schiene) \n Bild 2 = SchienenStrassenbus Museum (Schiene) \n Bild 3 = SchienenStrassenbus (Schiene) \n Bild 4 = SchienenStrassenbus Spurwagen (Schiene) \n Bild 5 = SchienenStrassenbus Prototyp (Straße) \n Bild 6 = SchienenStrassenbus Museum (Straße) \n Bild 7 = SchienenStrassenbus (Straße) \n.",
+		
+
+	},
+}
+end
